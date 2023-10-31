2d8e024dd0c1aa24976b03d8dc557f64ffe3d735
OksanaTryhub <108359208+OksanaTryhub@users.noreply.github.com>
2023-10-31 16:52:18 +0200

    Updated SigninForm

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -1,41 +1,64 @@
+import {Link, useNavigate} from 'react-router-dom';
 import { useForm } from "react-hook-form";
+
+import { useSignInAccount } from '@/lib/react-query/queriesAndMutations';
+import { useUserContext } from '@/context/AuthContext';
 
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import {Link} from 'react-router-dom';
+import { useToast } from "@/components/ui/use-toast"
 
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { SigninValidation, SignupValidation } from "@/lib/validation";
+import { SigninValidation} from "@/lib/validation";
 import Loader from "@/components/ui/shared/Loader";
 
 
-
 const SigninForm = () => {
-    const isLoading = false;
+    const { toast } = useToast();
+    const {checkAuthUser, isLoading: isUserLoading} = useUserContext();
+
+    const navigate = useNavigate();
+
+    const { mutateAsync: signInAccount} = useSignInAccount();
 
     // 1. Define your form.
-  const form = useForm<z.infer<typeof SigninValidation>>({
-    resolver: zodResolver(SigninValidation),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  })
- 
+    const form = useForm<z.infer<typeof SigninValidation>>({
+        resolver: zodResolver(SigninValidation),
+        defaultValues: {
+        email: "",
+        password: "",
+        },
+    })
+
   // 2. Define a submit handler.
-  async function onSubmit(values: z.infer<typeof SigninValidation>) {
-        //create new user
-        console.log(values)
-  }
+    async function onSubmit(values: z.infer<typeof SigninValidation>) {
+            const session = await signInAccount({
+            email: values.email,
+            password: values.password,
+        });
+
+        if (!session) {
+            return toast({title: "Sign in failed. Please, try again."})
+        }
+
+        const isLoggedIn = await checkAuthUser();
+        if(isLoggedIn) {
+            form.reset();
+
+            navigate('/');
+        } else {
+            return toast({title: "Sign in failed. Please, try again."})
+        }
+    }
 
   return (
     <Form {...form}>
         <div className="sm:w-420 flex-center flex-col">
             <img src="/assets/images/logo.svg" alt="logo" />
-            <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12 ">Welcome back</h2>
-            <p className="text-light-3 small-medium md:base-regular mt-2">To use Snapgram, please enter your details</p>
+            <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12 ">Log in to your account</h2>
+            <p className="text-light-3 small-medium md:base-regular mt-2">Welcome back! Please enter your details</p>
             <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
                 <FormField
                 control={form.control}
@@ -63,15 +86,15 @@ const SigninForm = () => {
                     </FormItem>
                 )}
                 />
-                <Button type="submit" className="shad-button_primary">
-                    {isLoading ? (
-                        <div className="flex-center gap-2">
-                            <Loader/> Loading...
-                        </div>
-                    ) : (
-                        "Sign up"
-                    )}
-                </Button>
+                 <Button type="submit" className="shad-button_primary">
+                        {isUserLoading ? (
+                            <div className="flex-center gap-2">
+                                <Loader/> Loading...
+                            </div>
+                        ) : (
+                            "Sign in"
+                        )}
+                    </Button>
 
                 <p className="text-small-regular text-light-2 text-center mt-2">
                     Don't have an account yet?
